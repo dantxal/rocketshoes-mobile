9aeb31e6a819f2be8489bbb5f3e02cce1680819e
dantxal <dantxal@gmail.com>
2019-09-20 19:55:42 -0300

    Renamed db file to host from my-json-server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.80:3333/',
+  baseURL: 'https://my-json-server.typicode.com/dantxal/rocketshoes-mobile',
 });
 
 export default api;
