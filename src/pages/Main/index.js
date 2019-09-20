16cdd0d5b899e4e7ea25cf7c130b51342d28461b
dantxal <dantxal@gmail.com>
2019-09-19 23:45:39 -0300

    api.get throwing error 0 Unhandled Promise Reject

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import { Container, ItemContainer } from './styles';
+import React, { Component } from 'react';
 
-export default function Main() {
-  return (
-    <Container>
-      <ItemContainer />
-      <ItemContainer />
-      <ItemContainer />
-    </Container>
-  );
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+import {
+  Loading,
+  Container,
+  ScrollableContainer,
+  ItemContainer,
+} from './styles';
+
+class Main extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      formattedPrice: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+    // this.getProducts();
+  }
+
+  getProducts = async () => {
+    const response = await api.get('/products').catch(err => {
+      throw err;
+    });
+
+    const data = response.data.map(product => ({
+      ...product,
+      formattedPrice: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  };
+
+  render() {
+    const { loading, products } = this.state;
+
+    return (
+      <Container>
+        <ScrollableContainer>
+          {loading ? (
+            <Loading />
+          ) : (
+            products.map(product => (
+              <ItemContainer>
+                {/* <ProductThumbnail />
+                <ProductTitle>{product.title}</ProductTitle>
+                <ProductPrice>{product.formatPrice} </ProductPrice>
+                <AddToCartButton>
+                  <CartAmount>
+                    <Icon />
+                    <Amount>2</Amount>
+                  </CartAmount>
+                  <AddToCartButtonText>
+                    ADICIONAR
+                  </AddToCartButtonText>
+                </AddToCartButton> */}
+              </ItemContainer>
+            ))
+          )}
+        </ScrollableContainer>
+      </Container>
+    );
+  }
 }
+
+export default Main;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,15 +1,27 @@
 import styled from 'styled-components/native';
 
+export const Loading = styled.ActivityIndicator`
+  color: #fff;
+  size: 48px;
+`;
+
 export const Container = styled.View`
   flex: 1;
   flex-direction: row;
   margin-top: 60px;
-  padding: 20px;
+  padding: 10px;
 `;
 
+export const ScrollableContainer = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})`
+  flex: 1;
+  height: 358px;
+`;
 export const ItemContainer = styled.View`
   background: #fff;
   height: 358px;
   width: 220px;
-  margin: 0 10px;
+  margin: 0 15px;
 `;
